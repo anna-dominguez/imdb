8407ac46e6820d6463aca740b81ea92cd25c8189
Vesta-nna <anna.dominguez.anna@gmail.com>
2023-09-22 18:53:57 +0200

    fetch data from tmdb api in home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,28 @@
-const Home = () => {
-  return <h1 className="text-red-400">HOME</h1>
+import Results from '@/components/Results'
+
+const API_KEY = process.env.API_KEY
+
+const Home = async ({ searchParams }) => {
+  const genre = searchParams.genre || 'fetchTrending'
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week'
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  )
+
+  const data = await res.json()
+  const results = data.results
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  )
 }
 
 export default Home
